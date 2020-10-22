e7bad62b35297acd1810b91c4cc22106824d3a55
Neeraj Shukla <neerajshukla1911@gmail.com>
2020-10-22 21:20:45 +0530

    mad git hook os independent

---
+++ scripts/copyhooks.js
@@ -0,0 +1,23 @@
+var exec = require('child_process').exec;
+var os = require('os');
+var cmd = "cp hooks/* .git/hooks/; chmod +x .git/hooks/*"
+var cmdWin = "xcopy .\\hooks\\* .\\.git\\hooks\\ /Y"
+var callback = function(error, stdout, stderr) {
+   if (error) {
+       console.log(`error: ${error.message}`);
+       return;
+   }
+   if (stderr) {
+       console.log(`stderr: ${stderr}`);
+       return;
+   }
+};
+if (os.type() === 'Linux') {
+   exec(cmd, callback);
+}
+else if (os.type() === 'Darwin') {
+   exec(cmd, callback);
+}
+else if (os.type() === 'Windows_NT') {
+   exec(cmdWin, callback);
+}
