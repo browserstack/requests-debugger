f6dfd11f2fb2efbf11b29b9e875ede179957b08e
Neeraj Shukla <neerajshukla1911@gmail.com>
2021-01-07 19:53:29 +0530

    used path module for joining paths

--- scripts/copyhooks.js
+++ scripts/copyhooks.js
@@ -1,11 +1,12 @@
-const fs = require('fs'); 
+const fs = require('fs');
+const path = require('path');
 
 const srcDir = './hooks/';
 const dstDir = './.git/hooks/'
 
 fs.readdirSync(srcDir).forEach(file => {
-  srcFile = srcDir + "/" + file;
-  dstFile = dstDir + "/" + file;
+  srcFile = path.join(srcDir, file);
+  dstFile = path.join(dstDir, file);
   fs.createReadStream(srcFile).pipe(fs.createWriteStream(dstFile));
   fs.chmodSync(srcFile , '755');
 });
